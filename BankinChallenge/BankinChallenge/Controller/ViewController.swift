f5b31f37b34e88fa8de0d97fb184e2e39b3f88cb
Alexandre <alex.legros60@gmail.com>
2021-12-22 09:50:26 +0100

    init project

---
+++ BankinChallenge/BankinChallenge/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankinChallenge
+//
+//  Created by Alexandre Legros on 22/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
