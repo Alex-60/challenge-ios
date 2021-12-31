42147945b1d12439b379b6dc9763dca2a44a0a54
Alexandre <alex.legros60@gmail.com>
2021-12-31 14:46:05 +0100

    add header for api call

---
+++ BankinChallenge/BankinChallenge/API Service/Header.swift
@@ -0,0 +1,18 @@
+//
+//  APIHeader.swift
+//  BankinChallenge
+//
+//  Created by Alexandre Legros on 22/12/2021.
+//
+
+import Foundation
+
+struct Header {
+
+    let headers: [String:String] = [
+        "Bridge-Version":"2021-06-01",
+        "Client-ID":"dd6696c38b5148059ad9dedb408d6c84",
+        "Client-Secret":"56uolm946ktmLTqNMIvfMth4kdiHpiQ5Yo8lT4AFR0aLRZxkxQWaGhLDHXeda6DZ"
+    ]
+}
+
